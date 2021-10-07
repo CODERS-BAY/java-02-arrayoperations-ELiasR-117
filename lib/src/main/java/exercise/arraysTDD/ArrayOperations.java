840b1ca334cbcf77229deaf61c607dd793b5dd9b
ELiasR-117 <78559780+ELiasR-117@users.noreply.github.com>
2021-10-07 14:46:30 +0200

    Tests

--- lib/src/main/java/exercise/arraysTDD/ArrayOperations.java
+++ lib/src/main/java/exercise/arraysTDD/ArrayOperations.java
@@ -22,7 +22,9 @@ public class ArrayOperations {
      */
 
     public void print() {
-        
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
     }
 
     /**
@@ -30,21 +32,48 @@ public class ArrayOperations {
      * @see <a href="sorting algortihms">http://faculty.cs.niu.edu/~hutchins/csci241/sorting.htm</a>
      */
     public int[] sort() {
-       return null;
+        int x=0;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 1; j < numbers.length; j++) {
+                if(numbers[j] < numbers[j-1]){
+                    x = numbers[j];
+                    numbers[j] = numbers[j-1];
+                    numbers[j-1] = x;
+                }
+            }
+        }
+        return numbers;
     }
 
     /**
      * @return the sorted array in reverse order
      */
     public int[] revertSort() {
-        return null;
+        int x=0;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = numbers.length-2; j>=0; j--) {
+                if(numbers[j] < numbers[j+1]){
+                    x = numbers[j+1];
+                    numbers[j+1] = numbers[j];
+                    numbers[j] = x;
+                }
+            }
+        }
+        return numbers;
     }
-
     /**
      * @return the unsorted array in reverted order.
      */
     public int[] reverted() {
-        return null;
+        int[] arr = new int[unsorted.length];
+        int x = 0;
+        for (int i = unsorted.length-1; i >= 0 ; i--) {
+            arr[x]=unsorted[i];
+            x++;
+
+        }
+        return arr;
+
     }
 
     /**
@@ -52,35 +81,80 @@ public class ArrayOperations {
      * @return true if the array contains the value, false otherwise.
      */
     public boolean contains(int value) {
+        for (int i = 0; i < numbers.length; i++) {
+            if(numbers[i] == value) {
+                return true;
+            }
+        }
         return false;
     }
-
     /**
      * @return the average value of all elements summed up.
      */
     public double average() {
-        return 0.0;
+        double sum = 0;
+        double avrg = 0;
+        for (int elem : numbers) {
+            sum+= elem;
+        }
+        return sum / numbers.length;
     }
 
     /**
      * @return the average value of all elements summed up, but without the highest and the lowest value.
      */
     public double trimmedMean() {
-        return 0.0;
+        int minValue = numbers[0];
+        int maxValue = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (minValue > numbers[i]) {
+                minValue = numbers[i];
+            }
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            if(maxValue < numbers[i]){
+                maxValue = numbers[i];
+            }
+        }
+        double sum = 0;
+        double x = 0;
+        for (int elem : numbers) {
+            sum+= elem;
+        }
+        x = maxValue + minValue;
+        sum = sum - x;
+
+        return sum / (numbers.length-2);
     }
+
+
+
+
 
     /**
      * @return the max value of the array. In the array [1,9,3] max would be 9.
      */
     public int maxValue() {
-        return 0;
+        int maxValue = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if(maxValue < numbers[i]){
+                maxValue = numbers[i];
+            }
+        }
+        return maxValue;
     }
 
     /**
      * @return the min value of the array. In the array [1,9,3] min would be 1.
      */
     public int minValue() {
-        return 0;
+        int minValue = numbers[0];
+        for (int i= 0; i < numbers.length; i++) {
+            if(minValue > numbers[i]){
+                minValue = numbers[i];
+            }
+        }
+        return minValue;
     }
 
     /* **********************************************************
